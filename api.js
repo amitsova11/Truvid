7eee8a127f4f5d50aa4b43b0342d7881d7e47a84
Amit Sova <amitsova@gmail.com>
2020-06-07 14:02:09 +0300

    no message

---
+++ api.js
@@ -0,0 +1,25 @@
+import "./index"
+
+function play() {
+    handlePlay();
+}
+
+function pause() {
+    handlePause();
+}
+
+function playNext() {
+    handleNext();
+}
+
+function playPrevious() {
+    handlePrevious();
+}
+
+function shuffle() {
+    initShuffleMode();
+}
+
+
+
+
